205f7a5fbf448697d765afdeaf71f1b6d5c2c947
Evan Wheeler <anon0mys87@gmail.com>
2020-01-19 14:08:31 -0700

    Working on converting temperature and light into rgb and brightness

---
+++ weather_globe_experiments/wg_hue.py
@@ -0,0 +1,9 @@
+# Code for Circuit Playground Express
+# This file needs to be renamed to code.py when
+# added to the playground express
+
+# This casts temperature in F to hue first to select a color
+# then converts hue to RGB to set the LEDs on the board.
+# Brightness of the lights is set by converting the value
+# read by the light sensor
+
---
+++ weather_globe_experiments/wg_rgb.py
@@ -0,0 +1,3 @@
+# Code for Circuit Playground Express
+# This file needs to be renamed to code.py when
+# added to the playground express
